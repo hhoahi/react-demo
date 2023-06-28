946baf2e593a3f3f93f17a910b1863824e02a7b2
Tran Thi Hanh Hoa <tthhoa.19it3@vku.udn.vn>
2023-06-28 15:59:58 +0700

    react-app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["New York", "Tokyo", "Ha Noi", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="City" />
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+// {items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  let items = ["New York", "Tokyo", "Ha Noi", "London", "Paris"];
-
+function ListGroup({ items, heading }: Props) {
   //Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   //   arr[0]; //variable (selectedIndex)
@@ -10,7 +13,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
